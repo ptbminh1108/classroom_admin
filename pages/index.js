6c261d16121fca4de92a86c3492407d8d5fda56e
ptbminh1108 <ptbminh1108@users.noreply.github.com>
2022-01-18 08:28:18 +0700

    update index homepage
    redirect when not login yet

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,36 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import TopBar from "../components/topbar/topBar";
 import Cookie from "js-cookie";
+import { useRouter } from "next/router";
+import {  useLayoutEffect } from "react";
 
 
 import { checkaccessToken } from "../services/user.service";
 export default function Home() {
+  const router = useRouter();
+
   const user = {};
   if (Cookie.get("user") != undefined) {
     user = JSON.parse(Cookie.get("user"));
     console.log("user");
     console.log(user);
   }
-  const access_token = Cookie.get("accesstoken");
+ 
 
   // const isLogin = checkaccessToken();
+  useLayoutEffect(() => {
+
+    Cookie.set("prePath", "/classroom");
+
+    if (!Cookie.get("accesstoken")) {
+      router.push("/login");
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
       <TopBar></TopBar>
-    
+
     </div>
   );
 }
